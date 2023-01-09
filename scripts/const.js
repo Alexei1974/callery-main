55b90b13c1fe2208d70942b56d985701d72fc83a
Alexei1974 <lelya.gaysin@mail.ru>
2023-01-09 10:06:39 +0300

    redirect url

--- scripts/const.js
+++ scripts/const.js
@@ -6,7 +6,7 @@ export const SECRET_KEY = 'cEYPv6vGRxOvgSjz73yd70MP7LIzeJt9YUnh56kS0Gs';
 export const API_URL_AUTH ='https://unsplash.com/oauth/authorize';
 export const API_URL_TOKEN = 'https://unsplash.com/oauth/token';
 
-export const REDIRECT_URI = 'http://127.0.0.1:5500/';
+export const REDIRECT_URI = 'https://callery-main.vercel.app/';
 export const RESPONSE_TYPE = 'code';
 export const SCOPE = 'public read_user read_photos write_likes';
 
